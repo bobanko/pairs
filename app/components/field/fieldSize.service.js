cf4202849d0b785dd446ee92f15431af5bf0a1d9
Vladimir Mishko <mishko.vladimir@gmail.com>
2017-07-07 20:15:04 +0300

    Field size set to static 8x8

--- app/components/field/fieldSize.service.js
+++ app/components/field/fieldSize.service.js
@@ -1,4 +1,7 @@
 export function getFieldSize() {
 	const apiUrl = 'https://kde.link/test/get_field_size.php';
-	return $.getJSON(apiUrl);
+	//return $.getJSON(apiUrl);
+
+	const size = {width: 8, height: 8};
+	return Promise.resolve(size);
 }
