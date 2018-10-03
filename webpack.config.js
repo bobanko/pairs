b954ea441b6f423ef3468eef8e23a6ddf455d382
Volodymyr Myshko <volodymyrm@wix.com>
2018-10-03 14:49:59 +0300

    build reconfigured for new webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,73 @@
-module.exports = {
-	mode: "development",
-	entry: { game: "./src/index.js" },
-	output: {
-		filename: "[name].bundle.js"
-	},
-	module: {
-		rules: [
-			{
-				test: /\.less$/,
-				use: [
-					{
-						loader: "style-loader"
-					},
-					{
-						loader: "css-loader",
-						options: {
-							sourceMap: true
-						}
-					},
-					{
-						loader: "less-loader",
-						options: {
-							sourceMap: true
-						}
-					}
-				]
-			},
-			{
-				test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-				use: [
-					{
-						loader: "url-loader",
-						options: {
-							limit: 12000
-						}
-					}
-				]
-			}
-		]
-	},
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-	devServer: {
-		//host: 'localhost', //default
-		//port: 8080, //default
-		//port: 9000
-		contentBase: "./dist",
-		compress: true
-		//hot: true
-	}
+const devMode = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  //mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[hash].js"
+  },
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all"
+        }
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 12000
+            }
+          }
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "less-loader"
+        ]
+      },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(["dist/*"]),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? "[name].css" : "[name].[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
+    })
+  ],
+
+  devServer: {
+    //host: 'localhost', //default
+    //port: 8080, //default
+    //port: 9000
+    contentBase: "./dist",
+    compress: true
+    //hot: true
+  }
 };
